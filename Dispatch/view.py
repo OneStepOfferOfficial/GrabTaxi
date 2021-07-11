daab5f85ce31ba6ebe154373ff55812073e3eed8
Connor-ZH <zhang_heng@nus.edu.cn>
2021-07-11 20:02:03 +0800

    Modify typo

--- Dispatch/view.py
+++ Dispatch/view.py
@@ -1,10 +1,10 @@
-from Dispatch import controller as controler
+from Dispatch import controller as controller
 
 
 def create_trip(location_pickup,location_dropoff,user_name):
-    trip_id = controler.store_trip(user_name,location_pickup,location_dropoff)
+    trip_id = controller.store_trip(user_name,location_pickup,location_dropoff)
     return trip_id
 
 def get_driver_id(trip_id):
-    driver_id = controler.get_driver_id(trip_id)
+    driver_id = controller.get_driver_id(trip_id)
     return driver_id
