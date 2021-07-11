2d1ce89e4fefe17ffe0c6e6eef3a13cdbdee0009
Connor-ZH <zhang_heng@nus.edu.cn>
2021-07-11 20:49:21 +0800

    Simplify the DBhelper file

--- Dispatch/DBhelper.py
+++ Dispatch/DBhelper.py
@@ -21,3 +21,50 @@ def insert_trip(trip_id,user_id,pickup_location,dropoff_location):
     connection.commit()
     connection.close()
 
+def create_trip_table():
+    # create_trip_table
+    connection = build_connection()
+    cursor = connection.cursor()
+    query = "CREATE TABLE trip_table(" \
+            "trip_id TEXT PRIMARY KEY," \
+            "user_id BIGINT," \
+            "driver_id BIGINT," \
+            "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP," \
+            "finished_at TIMESTAMP," \
+            "status TEXT," \
+            "driver_id_refused BIGINT[]," \
+            "pickup_location DECIMAL[]," \
+            "dropoff_location DECIMAL[]" \
+            ");"
+    cursor.execute(query)
+    connection.commit()
+    connection.close()
+
+def create_user_table():
+    # create_user_table
+    connection = build_connection()
+    cursor = connection.cursor()
+    query = "CREATE TABLE user_table(" \
+            "driver_id BIGSERIAL PRIMARY KEY," \
+            "driver_name TEXT," \
+            "phone_number VARCHAR(8)," \
+            "password TEXT," \
+            "trip_id TEXT);"
+    cursor.execute(query)
+    connection.commit()
+    connection.close()
+
+def create_driver_table():
+    # create_driver_table
+    connection = build_connection()
+    cursor = connection.cursor()
+    query = "CREATE TABLE driver_table(" \
+            "driver_id BIGSERIAL PRIMARY KEY," \
+            "driver_name TEXT," \
+            "phone_number VARCHAR(8)," \
+            "password TEXT," \
+            "trip_id TEXT);"
+    cursor.execute(query)
+    connection.commit()
+    connection.close()
+
