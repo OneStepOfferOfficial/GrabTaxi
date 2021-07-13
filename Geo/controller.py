a665a3677d47cc7ed15c5a84129a887ad8b74aae
Connor-ZH <zhang_heng@nus.edu.cn>
2021-07-13 16:22:10 +0800

    detach functions from controller to util

--- Geo/controller.py
+++ Geo/controller.py
@@ -1,4 +1,5 @@
 from Geo.DBhelper import Helper
+from Geo.util import *
 
 DBhelper = Helper()
 
@@ -56,31 +57,6 @@ def sort_nearest_drivers(drivers):
     :param drivers
     :return: the top 10 drivers_id
     '''
-    def swap(l,index1,index2):
-        '''
-        swap the two given index element in the array and return the index2
-        :param l:
-        :param index1:
-        :param index2:
-        :return: index2
-        '''
-        l[index1],l[index2] = l[index2],l[index1]
-        return index2
-    def bubble_down(drivers,index):
-        left_child_index = index*2+1
-        right_child_index = index*2+2
-        if left_child_index < len(drivers):
-            if right_child_index < len(drivers):
-                if drivers[index].distance > drivers[left_child_index].distance\
-                or drivers[index].distance > drivers[right_child_index].distance:
-                    index = swap(drivers,index,left_child_index) \
-                    if drivers[left_child_index].distance < drivers[right_child_index].distance\
-                        else swap(drivers,index,right_child_index)
-                    bubble_down(drivers,index)
-            else:
-                if drivers[index].distance > drivers[left_child_index].distance:
-                    index = swap(drivers, index, left_child_index)
-                    bubble_down(drivers, index)
     for index in range((len(drivers)-2)//2,-1,-1):
         bubble_down(drivers,index)
     return drivers[0:10]
