3717751eff9c1861ba34c8e7f740fbfffd9a4d38
Connor-ZH <zhang_heng@nus.edu.cn>
2021-07-15 19:04:11 +0800

    replace wrong file

--- Driver/view.py
+++ Driver/view.py
@@ -1,2 +1,9 @@
-a = ord("2") - ord("0")
-print(a)
+from Driver import controller
+
+def update_driver_location(driver_id,longitude,latitude):
+    controller.update_driver_location(driver_id,longitude,latitude)
+
+def update_all_drivers_locations():
+    controller.update_all_drivers_locations()
+
+update_all_drivers_locations()
